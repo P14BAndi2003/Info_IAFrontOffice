091a700f5910f2b0f337e908e156ec4b954a9e1a
P14BAndi2003 <106509349+P14BAndi2003@users.noreply.github.com>
2023-05-06 21:43:37 +0300

    erg

--- resources/views/articles/retail.blade.php
+++ resources/views/articles/retail.blade.php
@@ -7,127 +7,13 @@
    <!--- basic page needs
    ================================================== -->
    <meta charset="utf-8">
-	<title>{{ $article->titre }}</title>
-	<meta name="description" content="{{ $article->description }}">  
+	<title>Tous les derniers articles de l'IA sur IA_Info</title>
+	<meta name="description" content="Un site spécialisé sur les informations sur l'IA | IA_Info est en voie de développement pour donner le plus d'expérience possible aux amoureux de l'informatique surtout de l'IA">  
 	<meta name="author" content="Andi Rakotonirina">
 
-   <!-- mobile specific metas<!DOCTYPE html>
-<!--[if IE 8 ]><html class="no-js oldie ie8" lang="en"> <![endif]-->
-<!--[if IE 9 ]><html class="no-js oldie ie9" lang="en"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!--><html class="no-js" lang="en"> <!--<![endif]-->
-<head>
-
-<!--- basic page needs
-================================================== -->
-<meta charset="utf-8">
- <title>Tous les derniers articles de l'IA sur IA_Info</title>
- <meta name="description" content="Un site spécialisé sur les informations sur l'IA | IA_Info est en voie de développement pour donner le plus d'expérience possible aux amoureux de l'informatique surtout de l'IA">  
- <meta name="author" content="Andi Rakotonirina">
-
-<!-- mobile specific metas
-================================================== -->
- <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
-  <!-- CSS
-================================================== -->
-<link rel="stylesheet" href="assets/css/base.css">
-<link rel="stylesheet" href="assets/css/vendor.css">  
-<link rel="stylesheet" href="assets/css/main.css">
-  
-<!-- script
-================================================== -->
- <script src="assets/js/modernizr.js"></script>
- <script src="assets/js/pace.min.js"></script>
-
-<!-- favicons
- ================================================== -->
- <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
- <link rel="icon" href="assets/favicon.ico" type="image/x-icon">
-
-</head>
-
-<body id="top">
-
- <!-- header 
-================================================== -->
-<header class="short-header">   
-
-	<div class="gradient-block"></div>	
-
-	<div class="row header-content">
-
-		<div class="logo">
-		  <a href="index.html">Author</a>
-	   </div>
-
-		<nav id="main-nav-wrap">
-			 <ul class="main-navigation sf-menu">
-												 
-			 </ul>
-		 </nav> <!-- end main-nav-wrap -->
-
-	 
-		
-	</div>     		
-	
-</header> <!-- end header -->
-
-<section id="page-header">
-	<div class="row current-cat">
-		<div class="col-full">
-			<h1>Tous les derniers articles de l'IA sur IA_Info</h1>
-			<h2>Un site spécialisé sur les informations sur l'IA | IA_Info est en voie de développement pour donner le plus d'expérience possible aux amoureux de l'informatique surtout de l'IA</h2>
-		</div>   		
-	</div>
-</section>
-<!-- masonry
-================================================== -->
-<section id="bricks">
-
-
-
-</section> <!-- end bricks -->
-
-
-<!-- footer
-================================================== -->
-<footer>
-
-
-   <div class="footer-bottom">
-	   <div class="row">
-
-		   <div class="col-twelve">
-			   <div class="copyright">
-				  <span>© Copyright 2023</span> 
-				  <span>Design by <a href="">IA_Info company</a></span>		         	
-			  </div>
-
-			  <div id="go-top">
-				 <a class="smoothscroll" title="Back to Top" href="#top"><i class="icon icon-arrow-up"></i></a>
-			  </div>         
-		   </div>
-
-	   </div> 
-   </div> <!-- end footer-bottom -->  
-
-</footer>  
-
-<!-- Java Script
-================================================== --> 
-<script src="assets/js/jquery-2.1.3.min.js"></script>
-<script src="assets/js/plugins.js"></script>
-<script src="assets/js/jquery.appear.js"></script>
-<script src="assets/js/main.js"></script>
-
-</body>
-
-</html>
+   <!-- mobile specific metas
    ================================================== -->
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
- 	<!-- CSS
-   ================================================== -->
 
  	<!-- CSS
    ================================================== -->
@@ -162,70 +48,51 @@
 	      </div>
 
 	   	<nav id="main-nav-wrap">
-			
+				<ul class="main-navigation sf-menu">
+													
+				</ul>
 			</nav> <!-- end main-nav-wrap -->
 
 		
+   		
    	</div>     		
    	
    </header> <!-- end header -->
+
+   <section id="page-header">
+   	<div class="row current-cat">
+   		<div class="col-full">
+   			<h1>Tous les derniers articles de l'IA sur IA_Info</h1>
+			   <h2>Un site spécialisé sur les informations sur l'IA | IA_Info est en voie de développement pour donner le plus d'expérience possible aux amoureux de l'informatique surtout de l'IA</h2>
+   		</div>   		
+   	</div>
+   </section>
+   <!-- masonry
+   ================================================== -->
+   <section id="bricks">
+
+   	<div class="row masonry">
+
+   		<!-- brick-wrapper -->
+         <div class="bricks-wrapper">
+
    
 
-   <!-- content
-   ================================================== -->
-   <section id="content-wrap" class="blog-single">
-   	<div class="row">
-   		<div class="col-twelve">
+   </section> <!-- end bricks -->
 
-   			<article class="format-standard">  
-
-   				<div class="content-media">
-<center><div class="post-thumb">
-							<img src="data:image/png;base64,{{ $article->image }}"> 
-						</div>  </center>
-					</div>
-
-					<div class="primary-content">
-
-						<h1 class="page-title">{{ $article->titre }}</h1>	
-
-						<ul class="entry-meta">
-							<li class="date">{{ $article->datecreation->format('d/m/Y') }}</li>						
-							<li class="cat"><a href="">{{ $article->auteur->username }}</a></li>				
-						</ul>						
-
-						
-						<h3 class="lead">{{$article->description}}</h3>
-						{!!$article->contenu!!}
-						
-		  	
-					</div> <!-- end entry-primary -->		  			   
-
-	  			
-
-				</article>
-   		
-
-			</div> <!-- end col-twelve -->
-   	</div> <!-- end row -->
-
-
-
-   </section> <!-- end content -->
-
-
+   
    <!-- footer
    ================================================== -->
    <footer>
 
-   	
+
       <div class="footer-bottom">
       	<div class="row">
 
       		<div class="col-twelve">
 	      		<div class="copyright">
 		         	<span>© Copyright 2023</span> 
-		         	<span>Design by <a href="">Someone</a></span>		         	
+		         	<span>Design by <a href="">IA_Info company</a></span>		         	
 		         </div>
 
 		         <div id="go-top">
@@ -238,10 +105,13 @@
 
    </footer>  
 
+   <!-- Java Script
+   ================================================== --> 
    <script src="assets/js/jquery-2.1.3.min.js"></script>
    <script src="assets/js/plugins.js"></script>
    <script src="assets/js/jquery.appear.js"></script>
    <script src="assets/js/main.js"></script>
+
 </body>
 
 </html>
