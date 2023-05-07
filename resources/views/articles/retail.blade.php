cd0fd4c7aeac77015e70f607c6ecff1ed4d6850d
P14BAndi2003 <106509349+P14BAndi2003@users.noreply.github.com>
2023-05-07 15:43:05 +0300

    ge

--- resources/views/articles/retail.blade.php
+++ resources/views/articles/retail.blade.php
@@ -8,21 +8,21 @@
 		<!-- Shareable -->
 		<title>{{ $article->titre }}</title>
 		<!-- Bootstrap -->
-		<link rel="stylesheet" href="scripts/bootstrap/bootstrap.min.css">
+		<link rel="stylesheet" href="assets/scripts/bootstrap/bootstrap.min.css">
 		<!-- IonIcons -->
-		<link rel="stylesheet" href="scripts/ionicons/css/ionicons.min.css">
+		<link rel="stylesheet" href="assets/scripts/ionicons/css/ionicons.min.css">
 		<!-- Toast -->
-		<link rel="stylesheet" href="scripts/toast/jquery.toast.min.css">
+		<link rel="stylesheet" href="assets/scripts/toast/jquery.toast.min.css">
 		<!-- OwlCarousel -->
-		<link rel="stylesheet" href="scripts/owlcarousel/dist/assets/owl.carousel.min.css">
-		<link rel="stylesheet" href="scripts/owlcarousel/dist/assets/owl.theme.default.min.css">
+		<link rel="stylesheet" href="assets/scripts/owlcarousel/dist/assets/owl.carousel.min.css">
+		<link rel="stylesheet" href="assets/scripts/owlcarousel/dist/assets/owl.theme.default.min.css">
 		<!-- Magnific Popup -->
-		<link rel="stylesheet" href="scripts/magnific-popup/dist/magnific-popup.css">
-		<link rel="stylesheet" href="scripts/sweetalert/dist/sweetalert.css">
+		<link rel="stylesheet" href="assets/scripts/magnific-popup/dist/magnific-popup.css">
+		<link rel="stylesheet" href="assets/scripts/sweetalert/dist/sweetalert.css">
 		<!-- Custom style -->
-		<link rel="stylesheet" href="css/style.css">
-		<link rel="stylesheet" href="css/skins/all.css">
-		<link rel="stylesheet" href="css/demo.css">
+		<link rel="stylesheet" href="assets/css/style.css">
+		<link rel="stylesheet" href="assets/css/skins/all.css">
+		<link rel="stylesheet" href="assets/css/demo.css">
 	</head>
 
 	<body class="skin-orange">
@@ -86,18 +86,17 @@
 		</footer>
 		<!-- End Footer -->
 
-		<script src="js/jquery.js"></script>
-		<script src="js/jquery.migrate.js"></script>
-		<script src="scripts/bootstrap/bootstrap.min.js"></script>
-		<script>var $target_end=$(".best-of-the-week");</script>
-		<script src="scripts/jquery-number/jquery.number.min.js"></script>
-		<script src="scripts/owlcarousel/dist/owl.carousel.min.js"></script>
-		<script src="scripts/magnific-popup/dist/jquery.magnific-popup.min.js"></script>
-		<script src="scripts/easescroll/jquery.easeScroll.js"></script>
-		<script src="scripts/sweetalert/dist/sweetalert.min.js"></script>
-		<script src="scripts/toast/jquery.toast.min.js"></script>
-		<script src="js/demo.js"></script>
-		<script src="js/e-magz.js"></script>
+		<script src="assets/js/jquery.js"></script>
+		<script src="assets/js/jquery.migrate.js"></script>
+		<script src="assets/scripts/bootstrap/bootstrap.min.js"></script>
+		<script src="assets/scripts/jquery-number/jquery.number.min.js"></script>
+		<script src="assets/scripts/owlcarousel/dist/owl.carousel.min.js"></script>
+		<script src="assets/scripts/magnific-popup/dist/jquery.magnific-popup.min.js"></script>
+		<script src="assets/scripts/easescroll/jquery.easeScroll.js"></script>
+		<script src="assets/scripts/sweetalert/dist/sweetalert.min.js"></script>
+		<script src="assets/scripts/toast/jquery.toast.min.js"></script>
+		<script src="assets/js/demo.js"></script>
+		<script src="assets/js/e-magz.js"></script>
 	</body>
 </html>
 
