43bd3c37658a92f95450c2e3acc1a72cf11f6129
P14BAndi2003 <106509349+P14BAndi2003@users.noreply.github.com>
2023-05-07 15:27:40 +0300

    f

--- resources/views/articles/home.blade.php
+++ resources/views/articles/home.blade.php
@@ -50,7 +50,7 @@
 			<!-- End nav -->
 		</header>
 
-		<section class="home">
+		<section class="page">
 			<div class="container">
 				<div class="row">
 					<div class="col-md-12 col-sm-12 col-xs-12" >
