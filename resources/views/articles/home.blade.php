89a148ab185ebe2143923b8e17c2ea317bd8283b
P14BAndi2003 <106509349+P14BAndi2003@users.noreply.github.com>
2023-05-07 14:39:43 +0300

    y-y

--- resources/views/articles/home.blade.php
+++ resources/views/articles/home.blade.php
@@ -118,16 +118,16 @@
 		</footer>
 		<!-- End Footer -->
 
-		<script src="js/jquery.js"></script>
-		<script src="js/jquery.migrate.js"></script>
-		<script src="scripts/bootstrap/bootstrap.min.js"></script>
-		<script src="scripts/jquery-number/jquery.number.min.js"></script>
-		<script src="scripts/owlcarousel/dist/owl.carousel.min.js"></script>
-		<script src="scripts/magnific-popup/dist/jquery.magnific-popup.min.js"></script>
-		<script src="scripts/easescroll/jquery.easeScroll.js"></script>
-		<script src="scripts/sweetalert/dist/sweetalert.min.js"></script>
-		<script src="scripts/toast/jquery.toast.min.js"></script>
-		<script src="js/demo.js"></script>
-		<script src="js/e-magz.js"></script>
+		<script src="assets/js/jquery.js"></script>
+		<script src="assets/js/jquery.migrate.js"></script>
+		<script src="assets/scripts/bootstrap/bootstrap.min.js"></script>
+		<script src="assets/scripts/jquery-number/jquery.number.min.js"></script>
+		<script src="assets/scripts/owlcarousel/dist/owl.carousel.min.js"></script>
+		<script src="assets/scripts/magnific-popup/dist/jquery.magnific-popup.min.js"></script>
+		<script src="assets/scripts/easescroll/jquery.easeScroll.js"></script>
+		<script src="assets/scripts/sweetalert/dist/sweetalert.min.js"></script>
+		<script src="assets/scripts/toast/jquery.toast.min.js"></script>
+		<script src="assets/js/demo.js"></script>
+		<script src="assets/js/e-magz.js"></script>
 	</body>
 </html>
