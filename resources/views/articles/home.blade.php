fc30ffab5f6a7e01fefc6a8875be70103a6b8d98
P14BAndi2003 <106509349+P14BAndi2003@users.noreply.github.com>
2023-05-07 14:27:19 +0300

    et

--- resources/views/articles/home.blade.php
+++ resources/views/articles/home.blade.php
@@ -1,180 +1,123 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-	<title>Tous les derniers articles de l'IA sur IA_Info</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-	<meta name="description" content="Un site spécialisé sur les informations sur l'IA | IA_Info est en voie de développement pour donner le plus d'expérience possible aux amoureux de l'informatique surtout de l'IA">  
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<meta name="description" content="Magz is a HTML5 & CSS3 magazine template is based on Bootstrap 3.">
+		<!-- Shareable -->
+		<title>Magz &mdash; Responsive HTML5 &amp; CSS3 Magazine Template</title>
+		<!-- Bootstrap -->
+		<link rel="stylesheet" href="assets/scripts/bootstrap/bootstrap.min.css">
+		<!-- IonIcons -->
+		<link rel="stylesheet" href="assets/scripts/ionicons/css/ionicons.min.css">
+		<!-- Toast -->
+		<link rel="stylesheet" href="assets/scripts/toast/jquery.toast.min.css">
+		<!-- OwlCarousel -->
+		<link rel="stylesheet" href="assets/scripts/owlcarousel/dist/assets/owl.carousel.min.css">
+		<link rel="stylesheet" href="assets/scripts/owlcarousel/dist/assets/owl.theme.default.min.css">
+		<!-- Magnific Popup -->
+		<link rel="stylesheet" href="assets/scripts/magnific-popup/dist/magnific-popup.css">
+		<link rel="stylesheet" href="assets/scripts/sweetalert/dist/sweetalert.css">
+		<!-- Custom style -->
+		<link rel="stylesheet" href="assets/css/style.css">
+		<link rel="stylesheet" href="assets/css/skins/all.css">
+		<link rel="stylesheet" href="assets/css/demo.css">
+	</head>
 
-	
-
- 	<!-- CSS
-   ================================================== -->
-   <link rel="stylesheet" href="assets/css/base.css">
-   <link rel="stylesheet" href="assets/css/vendor.css">  
-   <link rel="stylesheet" href="assets/css/main.css">
-     
-
-   <!-- favicons
-	================================================== -->
-	<link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
-	<link rel="icon" href="assets/favicon.ico" type="image/x-icon">
-
-</head>
-
-<body id="top">
-
-	<!-- header 
-   ================================================== -->
-   <header class="short-header">   
-
-   	<div class="gradient-block"></div>	
-
-   	<div class="row header-content">
-
-   		<div class="logo">
-	         <a href="index.html">Author</a>
-	      </div>
-
-	   	<nav id="main-nav-wrap">
-				<ul class="main-navigation sf-menu">
-													
-				</ul>
-			</nav> <!-- end main-nav-wrap -->
-
-		
-   		
-   	</div>     		
-   	
-   </header> <!-- end header -->
-
-   <section id="page-header">
-   	<div class="row current-cat">
-   		<div class="col-full">
-   			<h1>Tous les derniers articles de l'IA sur IA_Info</h1>
-			   <h2>Un site spécialisé sur les informations sur l'IA | IA_Info est en voie de développement pour donner le plus d'expérience possible aux amoureux de l'informatique surtout de l'IA</h2>
-   		</div>   		
-   	</div>
-   </section>
-   <!-- masonry
-   ================================================== -->
-   <section id="bricks">
-
-   	<div class="row masonry">
-
-   		<!-- brick-wrapper -->
-         <div class="bricks-wrapper">
-
-         	<div class="grid-sizer"></div>
-
-         	<div class="brick entry featured-grid animate-this">
-         		<div class="entry-content">
-         			<div id="featured-post-slider" class="flexslider">
-			   			<ul class="slides">
-						@foreach($articles as $article)
-				   			<li>
-				   				<div class="featured-post-slide">
-
-						   			<div class="post-background" style="background-image:url('data:image/png;base64,{{ $article->image }}');"></div>
-
-								   	<div class="overlay"></div>			   		
-
-								   	<div class="post-content">
-								   		<ul class="entry-meta">
-												<li>{{ $article->datecreation->format('d/m/Y')}}</li> 
-												<li><a href="#" >{{ $article->auteur->username }}</a></li>				
-											</ul>	
-
-								   		<h1 class="slide-title"><a href="{{route('articles.retail',['id' => $article->id, 'slug' => Str::slug($article->titre) ,'randch' => rand(10000, 99999)]) }}" title="">{{ $article->titre }}</a></h1> 
-								   	</div> 				   					  
-				   			
-				   				</div>
-				   			</li> <!-- /slide -->
-							 
-						@endforeach
-				   		
-
-				   		</ul> <!-- end slides -->
-				   	</div> <!-- end featured-post-slider -->        			
-         		</div> <!-- end entry content -->         		
-         	</div>
-			 @foreach($pagines as $pagine)
-         	<article class="brick entry format-standard animate-this">
-
-               <div class="entry-thumb">
-                  <a href="{{route('articles.retail',['id' => $pagine->id, 'slug' => Str::slug($pagine->titre) ,'randch' => rand(10000, 99999)]) }}" class="thumb-link">
-	                  <img src="data:image/png;base64,{{ $pagine->image }}" alt="building">             
-                  </a>
-               </div>
-
-               <div class="entry-text">
-               	<div class="entry-header">
-
-               		<div class="entry-meta">
-               			<span class="cat-links">
-               				<a href="#">{{$pagine->auteur->username}}</a> 
-							<p>	<a href="#">{{$pagine->datecreation->format('d/m/Y')}}</a>               				
-               			</p>
-               			</span>			
-               		</div>
-
-               		<h1 class="entry-title"><a href="{{route('articles.retail',['id' => $pagine->id, 'slug' => Str::slug($pagine->titre) ,'randch' => rand(10000, 99999)]) }}">{{$pagine->titre}}</a></h1>
-               		
-               	</div>
-						<div class="entry-excerpt">
-						{{$pagine->description}}	
+	<body class="skin-orange">
+		<header class="primary">
+			<div class="firstbar">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-3 col-sm-12">
+							<div class="brand">
+								<a href="index.html">
+									<img src="assets/favicon.ico" alt="Magz Logo">
+								</a>
+							</div>						
+						</div>
+						<div class="col-md-9 col-sm-12">
+							<h1>Tous les derniers articles de l'IA sur IA_Info</h1>
+							<p>Un site spécialisé sur les informations sur l'IA | IA_Info est en voie de développement pour donner le plus d'expérience possible aux amoureux de l'informatique surtout de l'IA</p>						
+						</div>
+					
 					</div>
-               </div>
+				</div>
+			</div>
 
-        		</article> <!-- end article -->
-
-				@endforeach
-
-
-         </div> <!-- end brick-wrapper --> 
 		
+			<!-- End nav -->
+		</header>
 
-   	</div> <!-- end row -->
-       <div class="row">
-       <nav class="pagination">
+		<section class="home"  style="margin-top: 20%;">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 col-sm-12 col-xs-12" >
+						
+						
+						<div class="line">
+							<div>Latest News</div>
+						</div>
+						<div class="row">
+							<div class="col-md-4 col-sm-6 col-xs-12">
+								<div class="row">
+									<article class="article col-md-12">
+										<div class="inner">
+											<figure>
+												<a href="single.html">
+													<img src="images/news/img10.jpg" alt="Sample Article">
+												</a>
+											</figure>
+											<div class="padding">
+												<div class="detail">
+													<div class="time">December 10, 2016</div>
+													<div class="category"><a href="category.html">Healthy</a></div>
+												</div>
+												<h2><a href="single.html">Duis aute irure dolor in reprehenderit in voluptate</a></h2>
+												<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+												<footer>
+													<a href="#" class="love"><i class="ion-android-favorite-outline"></i> <div>1263</div></a>
+													<a class="btn btn-primary more" href="single.html">
+														<div>More</div>
+														<div><i class="ion-ios-arrow-thin-right"></i></div>
+													</a>
+												</footer>
+											</div>
+										</div>
+									</article>
+							
+								</div>
 
-    </nav>
-</div>
+							
+							</div>
+							
+						
+					</div>
+						</div>
+				</div>
+				</div>
+				</div>
+		</section>
 
+		<!-- Start footer -->
+		<footer class="footer">
+			<div class="container">
+		
+				<div class="row">
+					<div class="col-md-12">
+						<div class="copyright">
+							COPYRIGHT &copy; MAGZ 2017. ALL RIGHT RESERVED.
+							<div>
+								Made with <i class="ion-heart"></i> by <a href="http://kodinger.com">Kodinger</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<!-- End Footer -->
 
-   </section> <!-- end bricks -->
-
-   
-   <!-- footer
-   ================================================== -->
-   <footer>
-
-
-      <div class="footer-bottom">
-      	<div class="row">
-
-      		<div class="col-twelve">
-	      		<div class="copyright">
-		         	<span>© Copyright 2023</span> 
-		         	<span>Design by <a href="">IA_Info company</a></span>		         	
-		         </div>
-
-		         <div id="go-top">
-		            <a class="smoothscroll" title="Back to Top" href="#top"><i class="icon icon-arrow-up"></i></a>
-		         </div>         
-	      	</div>
-
-      	</div> 
-      </div> <!-- end footer-bottom -->  
-
-   </footer>  
-
-   <!-- Java Script
-   ================================================== --> 
-   <script src="assets/js/jquery-2.1.3.min.js"></script>
-   <script src="assets/js/plugins.js"></script>
-   <script src="assets/js/main.js"></script>
-
-</body>
-
+		
+	</body>
 </html>
